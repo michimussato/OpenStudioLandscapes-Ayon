ee9f484ec32241f2e6483f0070cca74d5f5c9efb
Michael Mussato <michimussato@gmail.com>
2025-12-14 13:33:52 +0100

    47-refactor-to-config-based-landscapes: OpenStudioLandscapes-Ayon refactor

--- src/OpenStudioLandscapes/Ayon/assets.py
+++ src/OpenStudioLandscapes/Ayon/assets.py
@@ -464,6 +464,9 @@ def compose(
 def cmd_extend(
     context: AssetExecutionContext,
 ) -> Generator[Output[list[Any]] | AssetMaterialization | Any, Any, None]:
+    # Todo
+    #  - [ ] use `OpenStudioLandscapes.engine.base.ops.factories.factory_compose_scope__cmd`
+    #        instead
 
     ret = []
 
@@ -484,8 +487,11 @@ def cmd_extend(
 def cmd_append(
     context: AssetExecutionContext,
 ) -> Generator[Output[dict[str, list[Any]]] | AssetMaterialization | Any, Any, None]:
+    # Todo
+    #  - [ ] use `OpenStudioLandscapes.engine.base.ops.factories.factory_compose_scope__cmd`
+    #        instead
 
-    ret = {"cmd": [], "exclude_from_quote": []}
+    ret = {"cmd": [], "exclude_from_quote": ["$(which docker)"]}
 
     yield Output(ret)
 
