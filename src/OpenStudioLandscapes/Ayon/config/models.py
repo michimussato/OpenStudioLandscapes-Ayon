f478ac372de23ff6e455f77d04ea5a1f03eee6b5
Michael Mussato <michimussato@gmail.com>
2025-12-23 13:27:20 +0100

    47-refactor-to-config-based-landscapes: fully dynamic CONFIG_STR

--- src/OpenStudioLandscapes/Ayon/config/models.py
+++ src/OpenStudioLandscapes/Ayon/config/models.py
@@ -10,12 +10,12 @@ from pydantic import (
 
 LOGGER = get_dagster_logger(__name__)
 
+from OpenStudioLandscapes.engine.config.str_gen import get_config_str
 from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
 
 from OpenStudioLandscapes.Ayon import dist
 
 config_default = pathlib.Path(__file__).parent.joinpath("config_default.yml")
-CONFIG_STR = config_default.read_text()
 
 
 class Branches(enum.StrEnum):
@@ -25,8 +25,6 @@ class Branches(enum.StrEnum):
 class Config(FeatureBaseModel):
 
     feature_name: str = dist.name
-
-    definitions: str = "OpenStudioLandscapes.Ayon.definitions"
 
     docker_compose_override: pathlib.Path = Field(
         default=pathlib.Path(
@@ -127,3 +125,9 @@ class Config(FeatureBaseModel):
             )
         )
         return ret
+
+
+CONFIG_STR = get_config_str(
+    Config=Config,
+)
+
