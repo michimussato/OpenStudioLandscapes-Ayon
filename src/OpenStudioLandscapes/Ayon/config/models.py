93ab827d9eb96d7906b0a27d51626633a9d97625
Michael Mussato <michimussato@gmail.com>
2026-05-04 14:28:37 +0200

    nox (readmes failed)

--- src/OpenStudioLandscapes/Ayon/config/models.py
+++ src/OpenStudioLandscapes/Ayon/config/models.py
@@ -2,15 +2,13 @@ import enum
 import pathlib
 from typing import List
 
+from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
+from OpenStudioLandscapes.engine.logging.loggers import FEATURE_LOGGER as LOGGER
 from pydantic import (
     Field,
     HttpUrl,
     PositiveInt,
 )
-
-from OpenStudioLandscapes.engine.logging.loggers import FEATURE_LOGGER as LOGGER
-
-from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
 
 from OpenStudioLandscapes.Ayon import constants, dist
 
@@ -132,6 +130,7 @@ if __name__ == "__main__":
     CONFIG_STR = Config.get_docs()
 else:
     import yaml
+
     CONFIG_STR = yaml.dump(
         Config.model_json_schema(mode="serialization"),
     )
